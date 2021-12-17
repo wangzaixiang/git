4d69ca1552422263c8bc45f2d4edec8b5c66e76c
wangzaixiang <949631531@qq.com>
2021-12-17 19:21:51 +0800

    hack log command

--- diff.c
+++ diff.c
@@ -4114,6 +4114,8 @@ object_read:
 	return 0;
 }
 
+int decode_km(char *path, void *data, unsigned long size, void **decoded_data,
+	      unsigned long *decoded_size);
 // TODO wzx here populate info into *s, hack here for decoded data
 // s->data, s->size
 int diff_populate_filespec(struct repository *r,
@@ -4123,11 +4125,105 @@ int diff_populate_filespec(struct repository *r,
 	if(err == 0){
 		// test for .java file only
 		// update s->data && s->size for decode
+		if(ends_with(s->path, ".java") && buffer_is_binary(s->data, s->size)){
+			void *buf;
+			unsigned long size;
+			if(decode_km(s->path, s->data, s->size, &buf, &size) == 0){
+				free(s->data);
+				s->data = buf;
+				s->size = size;
+			}
+		}
 		return 0;
 	}
 	else {
 		return err;
 	}
+}
+int do_write(int fd, char *data, unsigned long size);
+int do_read(int fd, void **data, unsigned long *size);
+int decode_km(char *path, void *data, unsigned long size, void **decoded_data,
+	      unsigned long *decoded_size)
+{
+	int child_stdin[2], child_stdout[2];
+	if(pipe(child_stdin) < 0){
+		return -1;
+	}
+	if(pipe(child_stdout) < 0) {
+		close(child_stdin[0]);
+		close(child_stdin[1]);
+		return -2;
+	}
+
+	pid_t child = fork();
+	if(child == 0){
+		// stdin
+		close(child_stdin[1]);
+		dup2(child_stdin[0], 0);
+		// stdout
+		dup2(child_stdout[1], 1);
+		close(child_stdout[0]);
+		char * args[] = { "gocat", NULL};
+		if(execvp("gocat", args) < 0){
+			perror("can't exec command gocat");
+			exit(100);
+		}
+	}
+	else if(child > 0){
+		close(child_stdin[0]);
+		close(child_stdout[1]);
+		do_write(child_stdin[1], (char*)data, size);
+		close(child_stdin[1]);
+
+		void *buf;
+		unsigned  long size;
+		do_read(child_stdout[0], &buf, &size);
+
+		int status = 0;
+		if(waitpid(child, &status, 0) <0){
+			close(child_stdout[0]);
+			return -3;
+		}
+		if(WIFEXITED(status)){
+			return -1 * WEXITSTATUS(status);
+		}
+		else {
+			return -4;
+		}
+	}
+	return 0;
+}
+int do_read(int fd, void **data, unsigned long *size)
+{
+	struct strbuf buffer;
+	int length = 0;
+
+	strbuf_init(&buffer, 1024);
+
+	char line[1024];
+	for(;;){
+		int n = read(fd, line, sizeof(line));
+		if(n < 0) return -1;
+		else if(n == 0) break;
+		else {
+			strbuf_grow(&buffer, n);
+			strbuf_add(&buffer, line, n);
+		}
+	}
+	*data = (void*)strbuf_detach(&buffer, size);
+	strbuf_release(&buffer);
+	return 0;
+}
+
+int do_write(int fd, char *data, unsigned long size)
+{
+	long writed = 0;
+	while(writed < size){
+		int n = write(fd, data + writed, size - writed);
+		if(n == -1) return -1;
+		writed += n;
+	}
+	return 0;
 }
 
 void diff_free_filespec_blob(struct diff_filespec *s)
